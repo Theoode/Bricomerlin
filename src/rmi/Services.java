070935640fd5a930b709a42b2bbb2be816c7ba67
theo <octheo.quennehen@gmail.com>
2025-03-27 11:51:33 +0100

    Connexion Base JDBC

--- src/rmi/Services.java
+++ src/rmi/Services.java
@@ -1,4 +1,15 @@
 package rmi;
 
-public class Services {
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+// Créer l'interface de l'objet distant
+public interface Services extends Remote {
+    String consulterStock(String reference) throws RemoteException;
+    List<String> rechercherArticlesParFamille(String famille) throws RemoteException;
+    boolean acheterArticle(String reference, int quantite) throws RemoteException;
+    String consulterFacture(int idFacture) throws RemoteException;
+    double calculerChiffreAffaire(String date) throws RemoteException;
+    boolean ajouterStockProduit(String reference, int quantite) throws RemoteException;
 }
