488e17337ca12b4883d81c613b28733d70c423dd
theo <octheo.quennehen@gmail.com>
2025-03-27 12:47:13 +0100

    Création du RMI , du client et du serveur

--- src/client/ClientApp.java
+++ src/client/ClientApp.java
@@ -8,31 +8,9 @@ import java.util.List;
 public class ClientApp {
     public static void main(String[] args) {
         try {
-            // Recherche du service sur le registre RMI
+
             Services service = (Services) Naming.lookup("rmi://localhost/ServiceStock");
-
             System.out.println(" Connexion au serveur RMI réussie !");
-
-            // Appel des méthodes distantes
-            System.out.println("🔹 Consultation du stock (A123) : " + service.consulterStock("A123"));
-
-            List<String> articles = service.rechercherArticlesParFamille("Electro");
-            System.out.println("🔹 Articles de la famille 'Electro' : " + articles);
-
-            // Acheter un article
-            boolean achat = service.acheterArticle("A123", 2);
-            System.out.println("🔹 Achat réussi ? " + achat);
-
-            // Vérification du stock après achat
-            System.out.println("🔹 Stock après achat : " + service.consulterStock("A123"));
-
-            // Ajout de stock
-            service.ajouterStockProduit("A123", 5);
-            System.out.println("🔹 Stock après ajout : " + service.consulterStock("A123"));
-
-            // Consultation du chiffre d'affaires
-            double chiffreAffaire = service.calculerChiffreAffaire("2025-03-27");
-            System.out.println("🔹 Chiffre d'affaires du jour : " + chiffreAffaire + "€");
 
         } catch (Exception e) {
             System.err.println("Erreur dans le client : " + e.getMessage());
--- src/server/ServeurRMI.java
+++ src/server/ServeurRMI.java
@@ -8,14 +8,11 @@ import java.rmi.registry.LocateRegistry;
 public class ServeurRMI {
     public static void main(String[] args) {
         try {
-            // Lancer le registre RMI sur le port 1099
+
             LocateRegistry.createRegistry(1099);
             System.out.println("Registre RMI démarré sur le port 1099...");
 
-            // Créer une instance du service
             Services service = new ServerImpl();
-
-            // Enregistrer l'objet distant dans le registre
             Naming.rebind("rmi://localhost/ServiceStock", service);
             System.out.println("Serveur prêt et service 'ServiceStock' enregistré...");
         } catch (Exception e) {
