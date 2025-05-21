d726140447f5970081dbb9bb259c8cf28761aa08
theo <octheo.quennehen@gmail.com>
2025-05-21 10:30:43 +0200

    Modifs implémentation méthodes

--- src/utils/DBManager.java
+++ src/utils/DBManager.java
@@ -7,9 +7,8 @@ import java.sql.SQLException;
 public class DBManager {
     private static final String URL = "jdbc:mysql://localhost:3306/bricomerlin";
     private static final String USER = "root";
-    private static final String PASSWORD = ""; // ton mot de passe ici
+    private static final String PASSWORD = "";
 
-    // Méthode statique pour récupérer une connexion
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
